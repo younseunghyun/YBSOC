0151cc5ce231470310603ab9dadbac9c811a1bc5
Jack SeungHyun Yoon <rogve22@gmail.com>
2014-12-17 21:38:29 +0900

    blogCrawlerCompleted

--- crawler/main.py
+++ crawler/main.py
@@ -1,18 +1,15 @@
 __author__ = 'jack'
 from multiprocessing import Process, Queue, Pool
-from urlCrawler import blogUrl
-from contextGather import blogContextGather
-import time
-from multiThreadCrawler import multithreadCrawler
+from urlCrawler import blogUrlC 
+from dataSaver import dataSaver
 
-if __name__ == "__main__":
-    crawler = multithreadCrawler()
-    crawler.keyword ='부산'
+ if __name__ =='__main__'   
     crawler.ini_date = '2013,1,1'
     crawler.e_date = '2014,10,7'
     crawler.thread_number = 10
     crawler.contextGather = blogContextGather
     crawler.urlGather = blogUrl
+    crawler.dataSaver = dataSaver.dataSaving
     print(crawler)
     crawler.run()
 
